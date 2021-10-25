a9c795461037b2827e0a1041e6748687e5b9c0b7
zFriez <65882932+MatheusSaar@users.noreply.github.com>
2021-10-24 22:31:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  github: 'MatheusSaar',
+  youtube: 'zFriez',
+  facebook: 'zFriez',
+  twitter: 'zFriez',
+  instagram: 'zFriez'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userGitHubLink.href = data.html_url
+      gitHubUserName.textContent = data.login
+      userImage.src = data.avatar_url
+    })
+}
+
+getGitHubProfileInfos()
